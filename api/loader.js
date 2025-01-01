c15bea2ad82d44b5770be3946634762dda589660
JosueGonzalezMontiel <143673540+JosueGonzalezMontiel@users.noreply.github.com>
2025-01-01 06:55:13 -0600

    6.1
    modificaciones para activar el servidor desde que se abra la web y tiempo de carga de facturas igual para hacer mas tiempo

--- api/loader.js
+++ api/loader.js
@@ -1,4 +1,6 @@
-window.onload = function(){
-    $('#onload').fadeOut();
-    $('body').removeClass('hidden');
-}
+window.onload = function() {
+    setTimeout(() => {
+        $('#onload').fadeOut();
+        $('body').removeClass('hidden');
+    }, 9000); // 5000 milisegundos = 5 segundos
+};
